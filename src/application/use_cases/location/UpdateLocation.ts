a447029eda4722ecd050e7dbc5dbafdb01e22e5f
Armando Monreal <acalderon.root@gmail.com>
2023-06-21 20:38:30 -0600

    Implement Auth

---
+++ src/application/use_cases/location/UpdateLocation.ts
@@ -0,0 +1,36 @@
+import { LocationEntity } from '../../../domain/entities/Location';
+import { ILocationRepository } from '../../../domain/Repository/location/ILocationRepository';
+
+import { ExceptionCode, ExceptionMessages } from '../../../domain/exceptions/ExceptionMessages';
+
+export default async (
+  id: string | number,
+  { name, longitude, latitude, address, mail, opinions, phone, storeHours },
+  locationRepository: ILocationRepository
+) => {
+  const exist: any | LocationEntity = await locationRepository.get(id);
+
+  if (!exist) {
+    throw new Error(ExceptionMessages[ExceptionCode.ENTITY_NOT_FOUND]);
+  }
+
+  const location = new LocationEntity(
+    id,
+    name || exist.name,
+    longitude || exist.longitude,
+    latitude || exist.latitude,
+    address || exist.address,
+    mail || exist.mail,
+    opinions || exist.opinions,
+    phone || exist.phone,
+    storeHours || exist.storeHours,
+  );
+
+  const updated = await locationRepository.update(location);
+
+  if (!updated) {
+    throw new Error(ExceptionMessages[ExceptionCode.BAD_REQUEST]);
+  }
+
+  return updated;
+};
